3b7adc5edc95867c5ac8bd9477b681ed8f3cd214
islam-2412 <islam2412@hotmail.com>
2026-09-11 14:31:14 +0300

    Update installation messages in installer.sh

--- fury/installer.sh
+++ fury/installer.sh
@@ -77,7 +77,7 @@ install_bitrate_if_available() {
 # ==============================================================================
 clear
 print_divider
-echo -e "${GREEN}                      Installing Fury-FHD Skin      ${NC}"
+echo -e "${GREEN}                      Installing Fury-FHD Skin 2025      ${NC}"
 echo -e "${YELLOW}                      Islam Salama (Abou Yassin)               ${NC}"
 print_divider
 echo ""
@@ -290,7 +290,7 @@ fi
 # ==============================================================================
 print_divider
 echo -e "${GREEN}              Fury-FHD${VERSION_LABEL} Installed/Updated Successfully!  ${NC}"
-echo -e "${MAGENTA}                           Long live Egypt.               ${NC}"
+echo -e "${YELLOW}                            Long live Egypt.               ${NC}"
 echo -e "${CYAN}             Please restart your Enigma2 GUI to apply changes.          ${NC}"
 print_divider
 exit 0
